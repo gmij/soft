11a752f0965ab0eb9cee8532af27552108e8f2ad
Gmij <gmij@qq.com>
2025-12-01 12:42:58 +0800

    Revert "Remove Google Fonts, add SEO pre-rendering, and add local font hosting"

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// Import Noto Sans SC font with specific weights
-import '@fontsource/noto-sans-sc/400.css'
-import '@fontsource/noto-sans-sc/500.css'
-import '@fontsource/noto-sans-sc/700.css'
 import './index.css'
 import App from './App.tsx'
 
